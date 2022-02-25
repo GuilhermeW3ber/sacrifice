43ea4f2ad01ba6fe8a4765cc8a5c18d2a148884b
broco <brococaxi@hotmail.com>
2022-02-25 16:59:30 -0300

    commit 3

--- sketch.js
+++ sketch.js
@@ -1,66 +1,76 @@
-var balloon,balloonImage1,balloonImage2;
+var balloon, balloonImage1, balloonImage2;
 var database;
 var height;
+var prota;
+var trees;
+var houses;
+var fences;
+var background;
+var monsters;
+var house_Img, house;
+var invisibleWall1,invisibleWall2,invisibleWall3,invisibleWall4
 
-function preload(){
-   bg =loadImage("Images/cityImage.png");
-   balloonImage1=loadAnimation("cube.png");
-  }
+function preload() {
+  house_Img=loadImage("house1.png")
+}
 
 function setup() {
+createCanvas(1600, 1600);
 
-   database=firebase.database();
+invisibleWall1=createSprite(800,0,1600,20);invisibleWall1.visible=false; 
+invisibleWall1.visible=false;  
 
-  createCanvas(1500,700);
+invisibleWall2=createSprite(0,800,20,1600);
+invisibleWall2.visible=false;
 
-  balloon=createSprite(250,650,150,150);
-  balloon.addAnimation("cube",balloonImage1);
-  balloon.scale=0.2;
+invisibleWall3=createSprite(1600,800,20,16000);
+invisibleWall3.visible=false;
 
-  var balloonHeight=database.ref('balloon/height');
-  balloonHeight.on("value",readHeight, showError);
+invisibleWall4=createSprite(800,1600,1600,20);
+invisibleWall4.visible=false;
 
-  textSize(20); 
+house=new House(house_Img, 350, 200);
+
+prota = createSprite(400, 400, 10, 10);
+prota.shapeColor = "blue";
+
 }
 
 function draw() {
-  background("gray");
-
-  if(keyDown(LEFT_ARROW)){
-    updateHeight(-10,0);
-  }
-  else if(keyDown(RIGHT_ARROW)){
-    updateHeight(10,0);
-  }
-  else if(keyDown(UP_ARROW)){
-    updateHeight(0,-10);
-  }
-  else if(keyDown(DOWN_ARROW)){
-    updateHeight(0,+10);
-  }
-
+  background(rgb(160,179,70));
   drawSprites();
-  fill(0);
-  stroke("white");
-  textSize(25);
-  text("USE ARROW KEYS TO MOVE THE CUBE",40,40);
+
+  prota.collide(invisibleWall1);
+  prota.collide(invisibleWall2);
+  prota.collide(invisibleWall3);
+  prota.collide(invisibleWall4);
+
+  if (keyIsDown(87)) {
+    prota.position.y = prota.position.y - 10
+    console.log("tecla");
+  }
+  if (keyIsDown(83)) {
+    prota.position.y = prota.position.y + 10
+    console.log("tecla");
+  }
+  if (keyIsDown(68)) {
+    prota.position.x = prota.position.x + 10
+    console.log("tecla");
+  }
+  if (keyIsDown(65)) {
+    prota.position.x = prota.position.x - 10
+    console.log("tecla");
+  }
+
+  if(mousePressed){
+    console.log("x: "+mouseX+" y: "+mouseY);
+  }
+  camera(0, 0, 20 + sin(frameCount * 0.01) * 10, 0, 0, 0, 0, 1, 0);
+  plane(10, 10);
+  camera.position.x=1000
+
 }
 
- function updateHeight(x,y){
-   database.ref('balloon/height').set({
-     'x': height.x + x ,
-     'y': height.y + y
-   })
- }
+function mousePressed(){
 
- function readHeight(data){
-   height = data.val();
-   balloon.x = height.x;
-   balloon.y = height.y;
- }
-
-
-
-function showError(){
-  console.log("Erro ao escrever no banco de dados");
 }
