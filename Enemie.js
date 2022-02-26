1360c97a3f9b2597bfba083c71efc628fc91152f
broco <brococaxi@hotmail.com>
2022-02-26 19:32:08 -0300

    commit 3

--- Enemie.js
+++ Enemie.js
@@ -3,6 +3,8 @@ class House{
         this.image=image;
         this.sprite=createSprite(x, y);
         this.sprite.addImage(this.image);
+        //this.collide(prota);
+        prota.collide(this.sprite);
         this.x=x;
         this.y=y;
     }
--- sketch.js
+++ sketch.js
@@ -1,23 +1,25 @@
 var balloon, balloonImage1, balloonImage2;
 var database;
 var height;
-var prota;
+var prota, protaIdleAn,protaRunAn;
 var trees;
 var houses;
 var fences;
 var background;
 var monsters;
-var house_Img, house;
+var house_Img, house, house2, house3aw;
 var invisibleWall1,invisibleWall2,invisibleWall3,invisibleWall4
 
 function preload() {
-  house_Img=loadImage("house1.png")
+  house_Img=loadImage("house1.png");
+  protaRunAn=loadAnimation("protaAns/protaRun1.png","protaAns/protaRun2.png","protaAns/protaRun3.png","protaAns/protaRun4.png","protaAns/protaRun5.png","protaAns/protaRun6.png");
+  protaIdleAn=loadAnimation("protaAns/protaIdle1.png","protaAns/protaIdle2.png","protaAns/protaIdle3.png");
 }
 
 function setup() {
 createCanvas(1600, 1600);
 
-invisibleWall1=createSprite(800,0,1600,20);invisibleWall1.visible=false; 
+invisibleWall1=createSprite(800,0,1600,20);
 invisibleWall1.visible=false;  
 
 invisibleWall2=createSprite(0,800,20,1600);
@@ -29,7 +31,9 @@ invisibleWall3.visible=false;
 invisibleWall4=createSprite(800,1600,1600,20);
 invisibleWall4.visible=false;
 
-house=new House(house_Img, 350, 200);
+house=new House(house_Img, 10, 50);
+house2=new House(house_Img, 110, 50);
+house3=new House(house_Img, 210, 50);
 
 prota = createSprite(400, 400, 10, 10);
 prota.shapeColor = "blue";
@@ -38,16 +42,19 @@ prota.shapeColor = "blue";
 
 function draw() {
   background(rgb(160,179,70));
-  drawSprites();
 
   prota.collide(invisibleWall1);
   prota.collide(invisibleWall2);
   prota.collide(invisibleWall3);
   prota.collide(invisibleWall4);
 
+  prota.addAnimation("Idle", protaIdleAn);
+
+
   if (keyIsDown(87)) {
     prota.position.y = prota.position.y - 10
     console.log("tecla");
+    //prota.addaAnimation("run", protaRunAn);
   }
   if (keyIsDown(83)) {
     prota.position.y = prota.position.y + 10
@@ -65,10 +72,10 @@ function draw() {
   if(mousePressed){
     console.log("x: "+mouseX+" y: "+mouseY);
   }
-  camera(0, 0, 20 + sin(frameCount * 0.01) * 10, 0, 0, 0, 0, 1, 0);
-  plane(10, 10);
-  camera.position.x=1000
 
+  camera.position.x=800
+
+  drawSprites();
 }
 
 function mousePressed(){
